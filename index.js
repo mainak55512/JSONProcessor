bb078158c67ce894d0a04227f8f88aa67581145e
mainak55512 <mbhattacharjee432@gmail.com>
2024-06-02 17:37:44 +0530

    custom interpreter update

--- index.js
+++
@@ -1,6 +0,0 @@
-var  {Jproc} = require("./JSONProcessor");
-var data = require("./test.json");
-
-var jproc = new Jproc(data);
-var query = "id name language version where language='Galician' and version<1.5";
-console.log(jproc.queryParser(query));
