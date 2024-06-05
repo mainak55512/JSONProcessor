cf9192991b2b3641c7260ef996fb06ebdccd8552
mainak55512 <mbhattacharjee432@gmail.com>
2024-06-06 00:20:19 +0530

    further amendments on class based approach

--- JSONProcessor.js
+++ JSONProcessor.js
@@ -1,25 +1,27 @@
 import eval_query from "./lib/interpreter.js"
 
-export function Jproc(data) {
-    this.data = data;
-}
+export default class Jproc {
+    constructor(data) {
+        this.data = data;
+    }
+    queryParser(query) {
+        let queryStr = query.split("where")[1].trim();
+        let parms = query.split("where")[0].trim().split(" ");
 
-Jproc.prototype.queryParser = function(query) {
-    let queryStr = query.split("where")[1].trim();
-    let parms = query.split("where")[0].trim().split(" ");
-
-    var resultArr = [];
-    this.data.forEach(function(obj) {
-        //var result = Function(queryStr)();
-        var result = eval_query(obj, queryStr);
-        if (result) {
-            var resultObj = {};
-            for (var i = 0; i < parms.length; i++) {
-                resultObj[parms[i]] = obj[parms[i]];
+        var resultArr = [];
+        this.data.forEach(function(obj) {
+            //var result = Function(queryStr)();
+            var result = eval_query(obj, queryStr);
+            if (result) {
+                var resultObj = {};
+                for (var i = 0; i < parms.length; i++) {
+                    resultObj[parms[i]] = obj[parms[i]];
+                }
+                resultArr.push(resultObj);
             }
-            resultArr.push(resultObj);
-        }
-    });
-    return resultArr;
+        });
+        return resultArr;
+    }
 }
+
 
